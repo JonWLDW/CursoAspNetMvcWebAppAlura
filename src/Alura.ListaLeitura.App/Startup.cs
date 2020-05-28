dd9b7b252b573db38b036879d35cd99580b16f65
Jonatan Waldow <jonwldw@gmail.com>
2020-05-28 01:04:36 -0400

    Primeiro commit.

---
+++ src/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder applicationBuilder)
+		{
+			applicationBuilder.Run(ImprimirLivrosParaLer);
+		}
+
+		public Task ImprimirLivrosParaLer(HttpContext context)
+		{
+			var repo = new LivroRepositorioCSV();
+			return context.Response.WriteAsync(repo.ParaLer.ToString());
+		}
+	}
+}
