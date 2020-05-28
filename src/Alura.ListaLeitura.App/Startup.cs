6e8db131ad9e00bc3c4b31703c65a2b25501b153
Jonatan Waldow <jonwldw@gmail.com>
2020-05-28 13:36:39 -0400

    Inicio das implementações de roteamento.

--- src/Alura.ListaLeitura.App/Startup.cs
+++ src/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,7 +10,24 @@ namespace Alura.ListaLeitura.App
 	{
 		public void Configure(IApplicationBuilder applicationBuilder)
 		{
-			applicationBuilder.Run(ImprimirLivrosParaLer);
+			applicationBuilder.Run(FazerRoteamento);
+		}
+
+		public Task FazerRoteamento(HttpContext context)
+		{
+			var repo = new LivroRepositorioCSV();
+			var caminhos = new Dictionary<string, string>
+			{
+				{"/Livros/ParaLer", repo.ParaLer.ToString() },
+				{"/Livros/Lendo", repo.Lendo.ToString() },
+				{"/Livros/Lidos", repo.Lidos.ToString()}
+			};
+
+			var path = context.Request.Path;
+			if (caminhos.ContainsKey(path))
+				return context.Response.WriteAsync(caminhos[path]);
+
+			return context.Response.WriteAsync("Caminho inexistente.");
 		}
 
 		public Task ImprimirLivrosParaLer(HttpContext context)
