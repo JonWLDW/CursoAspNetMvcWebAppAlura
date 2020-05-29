e1e4bc9db5b07b1198965cfcf0d3ff477f16287f
Jonatan Waldow <jonwldw@gmail.com>
2020-05-29 13:24:22 -0400

    Alterada forma de roteamento para usar o padrão do .NET Core.

--- src/Alura.ListaLeitura.App/Startup.cs
+++ src/Alura.ListaLeitura.App/Startup.cs
@@ -1,32 +1,32 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
 {
 	public class Startup
 	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddRouting();
+		}
+
 		public void Configure(IApplicationBuilder applicationBuilder)
 		{
-			applicationBuilder.Run(FazerRoteamento);
+			var routerBuilder = new RouteBuilder(applicationBuilder);
+			routerBuilder.MapRoute("Livros/ParaLer", ImprimirLivrosParaLer);
+			routerBuilder.MapRoute("Livros/Lendo", ImprimirLivrosLendo);
+			routerBuilder.MapRoute("Livros/Lidos", ImprimirLivrosLidos);
+
+			var rotas = routerBuilder.Build();
+			applicationBuilder.UseRouter(rotas);
 		}
 
 		public Task FazerRoteamento(HttpContext context)
 		{
-			var repo = new LivroRepositorioCSV();
-			var caminhos = new Dictionary<string, RequestDelegate>
-			{
-				{"/Livros/ParaLer", ImprimirLivrosParaLer },
-				{"/Livros/Lendo", ImprimirLivrosLendo },
-				{"/Livros/Lidos", ImprimirLivrosLidos }
-			};
-
-			var path = context.Request.Path;
-			if (caminhos.ContainsKey(path))
-				return caminhos[path].Invoke(context);
-
 			context.Response.StatusCode = StatusCodes.Status404NotFound;
 			return context.Response.WriteAsync("Caminho inexistente.");
 		}
